09f2ae88a27ee1be100ac76a80e6c2baa2005855
Josh Henke <henke.josh@gmail.com>
2016-07-28 15:22:47 -0700

    Began creating SQLObject class

--- ORM/lib/01_sql_object.rb
+++ ORM/lib/01_sql_object.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
@@ -5,18 +6,36 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    unless @columns
+      cols = DBConnection.execute2(<<-SQL)
+        SELECT *
+        FROM "#{self.table_name}"
+      SQL
+      @columns = cols.first.map(&:to_sym)
+    end
+    @columns
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |item|
+        self.attributes[col] = item
+      end
+    end
+      #TODO -> make it so user doesn't have to call #finalize!
+      # at end of subclass creation!!!
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.name.tableize
   end
 
   def self.all
@@ -36,7 +55,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
@@ -54,4 +73,6 @@ class SQLObject
   def save
     # ...
   end
+
+
 end
