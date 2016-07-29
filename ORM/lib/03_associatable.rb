6665c3a1531086552676f59a1bff8d10531717bd
Josh Henke <henke.josh@gmail.com>
2016-07-29 15:33:15 -0700

    Finished basic associations

--- ORM/lib/03_associatable.rb
+++ ORM/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,34 +11,68 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    if @class_name == "Human"
+      return "humans"
+    else
+      @class_name.tableize
+    end
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase,
+      primary_key: "id".to_sym
+    }
+    defaults.merge!(options)
+
+    @foreign_key = defaults[:foreign_key]
+    @class_name = defaults[:class_name]
+    @primary_key = defaults[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      class_name: "#{name.to_s.singularize.camelcase}",
+      primary_key: "id".to_sym
+    }
+    defaults.merge!(options)
+
+    @foreign_key = defaults[:foreign_key]
+    @class_name = defaults[:class_name]
+    @primary_key = defaults[:primary_key]
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    define_method(name) do
+
+      @options = BelongsToOptions.new(name, options)
+      fk_val = self.send(@options.foreign_key)
+
+      @options.model_class.where(@options.primary_key => fk_val).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    define_method(name) do
+      @options = HasManyOptions.new(name, self.class.name, options)
+
+      fk_val = self.send(@options.primary_key)
+
+      @options.model_class.where(@options.foreign_key => fk_val)
+    end
   end
 
   def assoc_options
@@ -46,5 +81,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
