0466e7b6eeedd98c7f8504baf8be90ca8ffc33a2
Josh Henke <henke.josh@gmail.com>
2016-07-29 20:03:00 -0700

    Updated controller base w/ quick addition

--- rails/lib/controller_base.rb
+++ rails/lib/controller_base.rb
@@ -1,125 +1,39 @@
-# require 'active_support'
-# require 'active_support/core_ext'
-# require 'erb'
-# require 'active_support/inflector'
-# require_relative './session'
-# require_relative './flash'
-# require 'byebug'
-#
-# class ControllerBase
-#   attr_reader :req, :res, :params
-#
-#   # Setup the controller
-#   def initialize(req, res, route_params = {})
-#     @req = req
-#     @res = res
-#     @params = route_params.merge(@req.params)
-#     #TODO -> allow for defining routes as strings like normal Rails
-#   end
-#
-#   # Helper method to alias @already_built_response
-#   def already_built_response?
-#     # @flash.track_cycle if @flash
-#     @already_built_response
-#   end
-#
-#   # Set the response status code and header
-#   def redirect_to(url)
-#     raise "Double render!" if already_built_response?
-#
-#     @res['location'] = url
-#     @res.status = 302
-#
-#     session.store_session(@res)
-#     flash.store_flash(@res)
-#
-#     @already_built_response = true
-#     nil
-#   end
-#
-#   # Populate the response with content.
-#   # Set the response's content type to the given type.
-#   # Raise an error if the developer tries to double render.
-#   def render_content(content, content_type)
-#     raise "Double render!" if already_built_response?
-#     @res['Content-Type'] = content_type
-#     @res.write(content)
-#
-#     session.store_session(@res)
-#     flash.store_flash(@res)
-#
-#     @already_built_response = true
-#   end
-#
-#   # use ERB and binding to evaluate templates
-#   # pass the rendered html to render_content
-#   def render(template_name)
-#     # TODO uncomment line 44 so that the view path actually works!!
-#     # view_folder = self.class.name.underscore.chomp("_controller")
-#     view_folder = self.class.name.underscore
-#     dir_path = File.dirname(__FILE__)
-#     rest_path = "../views/#{view_folder}/#{template_name}.html.erb"
-#     full_path = [dir_path, rest_path].join("/")
-#     contents = File.read(full_path)
-#
-#     template = ERB.new(contents).result(binding)
-#     render_content(template, "text/html")
-#   end
-#
-#   # method exposing a `Session` object
-#   def session
-#     @session ||= Session.new(@req)
-#   end
-#
-#   def flash
-#     @flash ||= Flash.new(@req)
-#   end
-#
-#   # use this with the router to call action_name (:index, :show, :create...)
-#   def invoke_action(name)
-#     self.send(name)
-#     if !already_built_response?
-#       render(name)
-#     end
-#   end
-# end
-
-
-require 'byebug'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 require_relative './session'
 require_relative './flash'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res, route_params = {})
-    @req, @res = req, res
-    @params = route_params.merge(req.params)
-    @already_built_response = false
-    @@protect_from_forgery ||= false
+    @req = req
+    @res = res
+    @params = route_params.merge(@req.params)
+    #TODO -> allow for defining routes as strings like normal Rails
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    # @flash.track_cycle if @flash
     @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
-    raise "double render error" if already_built_response?
+    raise "Double render!" if already_built_response?
 
+    @res['location'] = url
     @res.status = 302
-    @res["Location"] = url
-
-    @already_built_response = true
 
     session.store_session(@res)
     flash.store_flash(@res)
 
+    @already_built_response = true
     nil
   end
 
@@ -127,36 +41,29 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    raise "double render error" if already_built_response?
-
-    @res.write(content)
+    raise "Double render!" if already_built_response?
     @res['Content-Type'] = content_type
-
-    @already_built_response = true
+    @res.write(content)
 
     session.store_session(@res)
     flash.store_flash(@res)
 
-    nil
+    @already_built_response = true
   end
-
-  # Phase 3
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    # TODO uncomment line 44 so that the view path actually works!!
+    # view_folder = self.class.name.underscore.chomp("_controller")
+    view_folder = self.class.name.underscore
     dir_path = File.dirname(__FILE__)
-    template_fname = File.join(
-      dir_path, "..",
-      "views", self.class.name.underscore, "#{template_name}.html.erb"
-    )
+    rest_path = "../views/#{view_folder}/#{template_name}.html.erb"
+    full_path = [dir_path, rest_path].join("/")
+    contents = File.read(full_path)
 
-    template_code = File.read(template_fname)
-
-    render_content(
-      ERB.new(template_code).result(binding),
-      "text/html"
-    )
+    template = ERB.new(contents).result(binding)
+    render_content(template, "text/html")
   end
 
   # method exposing a `Session` object
@@ -170,50 +77,9 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
-    if protect_from_forgery? && req.request_method != "GET"
-      check_authenticity_token
-    else
-      form_authenticity_token
-    end
-
     self.send(name)
-    render(name) unless already_built_response?
-
-    nil
-  end
-
-  def form_authenticity_token
-    @token ||= generate_authenticity_token
-    res.set_cookie('authenticity_token', value: @token, path: '/')
-    @token
-  end
-
-  protected
-
-  def self.protect_from_forgery
-    @@protect_from_forgery = true
-  end
-
-  private
-
-  attr_accessor :already_built_response
-
-  def controller_name
-    self.class.to_s.underscore
-  end
-
-  def protect_from_forgery?
-    @@protect_from_forgery
-  end
-
-  def check_authenticity_token
-    cookie = @req.cookies["authenticity_token"]
-    unless cookie && cookie == params["authenticity_token"]
-      raise "Invalid authenticity token"
+    if !already_built_response?
+      render(name)
     end
-  end
-
-  def generate_authenticity_token
-    SecureRandom.urlsafe_base64(16)
   end
 end
