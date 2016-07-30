77455cb3fc3a7a618453e1a632fc2f52e8999eef
Josh Henke <henke.josh@gmail.com>
2016-07-29 21:33:51 -0700

    Began working on exception handling Middleware

--- rails/lib/show_exceptions.rb
+++ rails/lib/show_exceptions.rb
@@ -1,15 +1,36 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+      @app.call(env)
+    rescue Exception => e
+      render_exception(e)
   end
 
   private
 
   def render_exception(e)
+    dir_path = File.dirname(__FILE__)
+    template_location = "#{dir_path}/templates/rescue.html.erb"
+    content = File.read(template_location)
+
+    body = ERB.new(content).result(binding)
+
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write(body)
+    res.status = 500
+    res.finish
+  end
+
+  def find_error_line(e)
+    trace_last = e.backtrace.first
   end
 
 end
